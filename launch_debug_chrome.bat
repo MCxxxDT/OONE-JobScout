fac87596324f05c9fca470e08ec28e61f19866c4
MCxxxDT <MCxxxDT@users.noreply.github.com>
2026-09-05 22:36:49 +0800

    chrome: 启动脚本与桌面快捷方式增加--start-maximized及BOSS聊天页直达,防无窗口幽灵运行（测试：语法检查+桌面快捷方式同步 通过）

--- launch_debug_chrome.bat
+++ launch_debug_chrome.bat
@@ -2,7 +2,7 @@
 set "CHROME=C:\Program Files\Google\Chrome\Application\chrome.exe"
 set "PROFILE=C:\Users\LENOVO\chrome-cdp-profile"
 
-start "" "%CHROME%" --remote-debugging-port=9335 --user-data-dir="%PROFILE%" --no-first-run --no-default-browser-check --disable-background-networking --remote-allow-origins=*
+start "" "%CHROME%" --remote-debugging-port=9335 --user-data-dir="%PROFILE%" --no-first-run --no-default-browser-check --disable-background-networking --remote-allow-origins=* --start-maximized "https://www.zhipin.com/web/geek/chat"
 
 echo.
 echo Verify: open http://127.0.0.1:9335/json/version in any browser tab.
